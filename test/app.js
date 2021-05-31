38ac90e81df4be6e315b9a1a0c7e7f05c04165ce
Alireza <alirezaaedalat@gmail.com>
2021-05-31 11:21:43 +0430

    the posts written by the user

--- test/app.js
+++ test/app.js
@@ -11,7 +11,7 @@ chai.should();
 chai.expect();
 chai.use(chaiHttp);
  /*
-  * Search for the user with username “Delphine”. /POST route
+  * Search for the user with username “Delphine”. /GET route
   */
 describe('Users APIs',() =>{
     describe('Search Delphine username /users',() =>{
@@ -21,27 +21,26 @@ describe('Users APIs',() =>{
             .get("/users?username="+user_name)
             .end((_err,response) => {
                 response.should.have.status(200);
-                response.body.should.be.a('array');
-                response.body[0].should.have.property("id");
-                response.body[0].should.have.property("name");
-                response.body[0].should.have.property("username");
-                response.body[0].should.have.property("email");
-                response.body[0].should.have.property("address");
-                response.body[0].address.should.have.property("street");
-                response.body[0].address.should.have.property("suite");
-                response.body[0].address.should.have.property("city");
-                response.body[0].address.should.have.property("zipcode");
-                response.body[0].address.should.have.property("geo");
-                response.body[0].address.geo.should.have.property("lat");
-                response.body[0].address.geo.should.have.property("lng");
-                response.body[0].should.have.property("phone");
-                response.body[0].should.have.property("website");
-                response.body[0].should.have.property("company");
-                response.body[0].should.have.property("company");
-                response.body[0].company.should.have.property("name");
-                response.body[0].company.should.have.property("catchPhrase");
-                response.body[0].company.should.have.property("bs");
-                expect(response.body[0].id).to.be.not.null;
+                response.body.should.be.a('array').to.be.not.null;
+                response.body[0].should.have.property("id").to.be.not.null;
+                response.body[0].should.have.property("name").to.be.not.null;
+                response.body[0].should.have.property("username").to.be.not.null;
+                response.body[0].should.have.property("email").to.be.not.null;
+                response.body[0].should.have.property("address").to.be.not.null;
+                response.body[0].address.should.have.property("street").to.be.not.null;
+                response.body[0].address.should.have.property("suite").to.be.not.null;
+                response.body[0].address.should.have.property("city").to.be.not.null;
+                response.body[0].address.should.have.property("zipcode").to.be.not.null;
+                response.body[0].address.should.have.property("geo").to.be.not.null;
+                response.body[0].address.geo.should.have.property("lat").to.be.not.null;
+                response.body[0].address.geo.should.have.property("lng").to.be.not.null;
+                response.body[0].should.have.property("phone").to.be.not.null;
+                response.body[0].should.have.property("website").to.be.not.null;
+                response.body[0].should.have.property("company").to.be.not.null;
+                response.body[0].should.have.property("company").to.be.not.null;
+                response.body[0].company.should.have.property("name").to.be.not.null;
+                response.body[0].company.should.have.property("catchPhrase").to.be.not.null;
+                response.body[0].company.should.have.property("bs").to.be.not.null;
                 expect(response.body[0].id).to.be.eql(9);
                 expect(response.body[0].username).to.be.eql("Delphine")
             done();    
@@ -50,3 +49,27 @@ describe('Users APIs',() =>{
         });
     });
 });
+/*
+  * Use the details fetched to make a search for the posts written by the
+user.”. /GET route
+  */
+describe('Posts APIs',() =>{
+    describe(' Details fetched /posts',() =>{
+        it("It should return all the posts written by the user ",(done) => {
+            const user_id  = 9;
+            chai.request(server)
+            .get("/posts?userId="+user_id)
+            .end((_err,response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('array');
+                response.body[0,1,2,3,4,5,6,7,8,9].should.have.property("userId").to.be.not.null
+                response.body[0,1,2,3,4,5,6,7,8,9].should.have.property("id").to.be.not.null
+                response.body[0,1,2,3,4,5,6,7,8,9].should.have.property("title").to.be.not.null
+                response.body[0,1,2,3,4,5,6,7,8,9].should.have.property("body").to.be.not.null
+                response.body[0,1,2,3,4,5,6,7,8,9].should.have.property("userId").eql(user_id);
+                
+            })
+            done();
+        })
+    })
+})
